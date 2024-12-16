a4306b4499406899f96964d509e4bd634fe91d5f
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-16 16:49:02 +0200

    add Home and Profile pages to routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
+import LeftBar from './components/leftBar/LeftBar.jsx';
+import Navbar from './components/navbar/navbar.jsx';
+import RightBar from './components/rightBar/rightBar.jsx';
 import Login from './pages/login/Login.jsx';
 import Register from './pages/register/Register.jsx';
+import Home from './pages/home/Home.jsx';
+import Profile from './pages/profile/Profile.jsx';
 import {
     createBrowserRouter,
     RouterProvider,
     Route,
+    Outlet,
 } from 'react-router-dom';
 
-const router = createBrowserRouter([
-    {
-        path: '/login',
-        element: <Login/>,
-    },
-    {
-        path: '/register',
-        element: <Register/>,
-    },
-]);
-
 function App() {
+
+    const Layout = () => {
+        return(
+            <div>
+                <Navbar/>
+                <div style={{display:"flex"}}>
+                    <LeftBar/>
+                    <Outlet/>
+                    <RightBar/>
+                </div>
+            </div>
+        )
+    }
+
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <Layout/>,
+            children: [
+                {
+                    path: '/', 
+                    element: <Home/>
+                }, 
+                {
+                    path: '/profile/:id', 
+                    element: <Profile/>
+                }
+            ]
+        },
+        {
+            path: '/login',
+            element: <Login/>,
+        },
+        {
+            path: '/register',
+            element: <Register/>,
+        },
+    ]);
+
     return (
         <div>
             <RouterProvider router={router}/>
