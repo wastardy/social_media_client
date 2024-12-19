dea6f3c3fe21c001280f0a42723fcde0d2a287fd
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-19 14:17:09 +0200

    add posts to main page

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,39 @@
+import './post.scss';
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Link } from 'react-router-dom';
+
+const Post = ({post}) => {
+    return (
+        <div className="post">
+            <div className="container">
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={post.profilePic} alt="" />
+                        <div className="details">
+                            <Link to={`/profile/${post.userId}`} style={{textDecoration:"none", color:"inherit"}}>
+                                <span className='name'>{post.name}</span>
+                            </Link>
+                            <span className='date'>3 mins ago</span>
+                        </div>
+                    </div>
+                    <MoreHorizIcon/>
+                </div>
+                <div className="content">
+                    <p>{post.desc}</p>
+                    <img src={post.img} alt="" />
+                </div>
+                <div className="info">
+                    <div className="item">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Post;
--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,8 +1,40 @@
 import './posts.scss';
+import Post from '../../components/post/Post';
 
 const Posts = () => {
+
+    const posts = [
+        {
+            id: 1,
+            name: "champagnepapi",
+            userId: 1,
+            profilePic: "./src/assets/drake.jpg",
+            desc: "From it's all blur tour",
+            img: "./src/assets/drake_post.png",
+        },
+        {
+            id: 2,
+            name: "ryanholiday",
+            userId: 2,
+            profilePic: "./src/assets/ryan_holiday.jpg",
+            desc: "Your potential, the absolute best you’re capable of—that’s tposthe metric to measure yourself against. Your standards are. Winning is not enough. People can get lucky and win. People can be assholes and win. Anyone can win. But not everyone is the best possible version of themselves.",
+        },
+        {
+            id: 3,
+            name: "cillianmurphyofficiall",
+            userId: 2,
+            profilePic: "./src/assets/cillian_murphy.png",
+            desc: "I'm kind of a little in shock",
+            img: "./src/assets/cillian_murphy_post.jpg",
+        },
+    ];
+
     return (
-        <div className="posts">Posts</div>
+        <div className="posts">
+            {posts.map(post => (
+                <Post post={post} key={post.id}/>
+            ))}
+        </div>
     );
 }
 
