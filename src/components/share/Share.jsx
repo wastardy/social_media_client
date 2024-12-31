bf413e88138655c2b2c22bc359ddf024feb11910
Andrew Kononenko <wastardy.k@gmail.com>
2024-12-31 14:23:47 +0200

    add share component (so that user can post images and text)

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,108 @@
+import "./share.scss";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage, faMapMarkerAlt, faUserFriends } from '@fortawesome/free-solid-svg-icons';
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/authContext.jsx";
+import { useMutation, useQueryClient } from "react-query";
+import { makeRequest } from "../../axios.js";
+
+const Share = () => {
+    const [file, setFile] = useState(null);
+    const [desc, setDesc] = useState("");
+
+    const upload = async () => {
+        try {
+            const formData = new FormData();
+
+            formData.append("file", file);
+
+            const res = await makeRequest.post("/upload", formData);
+
+            return res.data;
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    };
+
+    const { currentUser } = useContext(AuthContext);
+
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation(
+        (newPost) => {
+            return makeRequest.post("/posts", newPost);
+        },
+        {
+            onSuccess: () => {
+                // Invalidate and refetch
+                queryClient.invalidateQueries(["posts"]);
+            },
+        }
+    );
+
+    const handleClick = async (event) => {
+        event.preventDefault();
+
+        let imgUrl = "";
+        
+        if (file) imgUrl = await upload();
+        mutation.mutate({ desc, img: imgUrl });
+        setDesc("");
+        setFile(null);
+    };
+
+    return (
+        <div className="share">
+            <div className="container">
+                <div className="top">
+                    <div className="left">
+                        <img src={currentUser.profile_picture} alt="" />
+                        <input
+                            type="text"
+                            placeholder={`Any news to share, ${currentUser.name}?`}
+                            onChange={(e) => setDesc(e.target.value)}
+                            value={desc}
+                        />
+                    </div>
+                    <div className="right">
+                        {file && (
+                            <img className="file" alt="" src={URL.createObjectURL(file)} />
+                        )}
+                    </div>
+                </div>
+                <hr />
+
+                <div className="bottom">  
+                    <div className="left">
+                        <input
+                            type="file"
+                            id="file"
+                            style={{ display: "none" }}
+                            onChange={(e) => setFile(e.target.files[0])}
+                        />
+                        <label htmlFor="file">
+                            <div className="item">
+                                <FontAwesomeIcon icon={faImage} className="icon" />
+                                <span>Add Image</span>
+                            </div>
+                        </label>
+                        <div className="item">
+                            <FontAwesomeIcon icon={faMapMarkerAlt} className="icon" />
+                            <span>Add Place</span>
+                        </div>
+                        <div className="item">
+                            <FontAwesomeIcon icon={faUserFriends} className="icon" />
+                            <span>Tag Friends</span>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <button onClick={handleClick}>Share</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Share;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Stories from '../../components/stories/Stories.jsx';
+import Share from '../../components/share/Share.jsx'
 import Posts from '../../components/posts/Posts.jsx';
 import './home.scss';
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div className='home'>
             <Stories/>
+            <Share/>
             <Posts/>
         </div>
     )
